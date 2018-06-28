9bf8dc96d6eb848c1422e1f526a5e87d1a37fcff
Rishav Sharan <rishav.sharan@live.com>
2018-06-28 13:14:04 +0530

    Router: simplest router added

--- src/cove.cr
+++ src/cove.cr
@@ -10,8 +10,16 @@ module Cove
         HTTP::LogHandler.new,
     ]) do |context|
         context.response.content_type = "text/plain"
-        context.response.print "Hello world!"
+
+    route = context.request.resource
+    case route
+        when "/hola"
+            context.response.print "Yo buddy!"
+        else
+            context.response.print "Bewarsies! This be wasteland!"
+        end
     end
 
+    puts "Server Started! Listening on localhost:8080"
     server.listen(8080)
 end
