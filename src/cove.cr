7c538f7b2f456a9f6d22adc99edfe3e40e0a7c7a
Rishav Sharan <rishav.sharan@live.com>
2018-06-28 16:27:10 +0530

    Router: separated class

--- src/cove.cr
+++ src/cove.cr
@@ -5,6 +5,41 @@ require "./cove/*"
 
 # TODO: Write documentation for `Cove`
 module Cove
+    class Router
+        def self.run(method, url, ctx)
+            url = clean(url)
+            case url
+            when "/hola/"
+                ctx.response.print "Yo buddy!"
+            when "/json/"
+                ctx.response.content_type = "application/json"
+                json_string = JSON.build do |json|
+                    json.object do
+                        json.field "name", "foo"
+                        json.field "values" do
+                            json.array do
+                                json.number 1
+                                json.number 2
+                                json.number 3
+                            end
+                        end
+                    end
+                end
+                    
+                ctx.response.print json_string
+            else
+                ctx.response.print "Bewarsies! This be wasteland!"
+            end
+        end
+
+        def self.clean (url)
+            if !url.rstrip.ends_with?("/")
+                url + "/"
+            else
+                url
+            end
+        end
+    end
 
     server = HTTP::Server.new([
         HTTP::ErrorHandler.new,
@@ -15,32 +50,8 @@ module Cove
         route_url = context.request.resource
         route_method = context.request.method
 
-        if !route_url.rstrip.ends_with?("/")
-            route_url = route_url + "/"
-        end
+        Router.run(route_method, route_url, context)
 
-        case route_url
-        when "/hola/"
-            context.response.print "Yo buddy!"
-        when "/json/"
-            context.response.content_type = "application/json"
-            json_string = JSON.build do |json|
-                json.object do
-                    json.field "name", "foo"
-                    json.field "values" do
-                        json.array do
-                            json.number 1
-                            json.number 2
-                            json.number 3
-                        end
-                    end
-                end
-            end
-                
-            context.response.print json_string
-        else
-            context.response.print "Bewarsies! This be wasteland!"
-        end
     end
 
     puts "Server Started! Listening on localhost:8080"
