a656e48c39604cd3f8a09c368986a1571e387ea8
Rishav Sharan <rishav.sharan@live.com>
2018-06-28 16:14:37 +0530

    Router: simple json, url clean

--- src/cove.cr
+++ src/cove.cr
@@ -1,4 +1,5 @@
 require "http/server"
+require "json"
 
 require "./cove/*"
 
@@ -11,10 +12,32 @@ module Cove
     ]) do |context|
         context.response.content_type = "text/plain"
 
-    route = context.request.resource
-    case route
-        when "/hola"
+        route_url = context.request.resource
+        route_method = context.request.method
+
+        if !route_url.rstrip.ends_with?("/")
+            route_url = route_url + "/"
+        end
+
+        case route_url
+        when "/hola/"
             context.response.print "Yo buddy!"
+        when "/json/"
+            context.response.content_type = "application/json"
+            json_string = JSON.build do |json|
+                json.object do
+                    json.field "name", "foo"
+                    json.field "values" do
+                        json.array do
+                            json.number 1
+                            json.number 2
+                            json.number 3
+                        end
+                    end
+                end
+            end
+                
+            context.response.print json_string
         else
             context.response.print "Bewarsies! This be wasteland!"
         end
