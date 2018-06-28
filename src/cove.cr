1e185b3f20e707a9becee7799d540ba2a0ba39e1
Rishav Sharan <rishav.sharan@live.com>
2018-06-28 18:34:57 +0530

    Router: routes moved to new file

--- src/cove.cr
+++ src/cove.cr
@@ -5,42 +5,8 @@ require "./cove/*"
 
 # TODO: Write documentation for `Cove`
 module Cove
-    class Router
-        def self.run(method, url, ctx)
-            url = clean(url)
-            case url
-            when "/hola/"
-                ctx.response.print "Yo buddy!"
-            when "/json/"
-                ctx.response.content_type = "application/json"
-                json_string = JSON.build do |json|
-                    json.object do
-                        json.field "name", "foo"
-                        json.field "values" do
-                            json.array do
-                                json.number 1
-                                json.number 2
-                                json.number 3
-                            end
-                        end
-                    end
-                end
-                    
-                ctx.response.print json_string
-            else
-                ctx.response.print "Bewarsies! This be wasteland!"
-            end
-        end
 
-        def self.clean (url)
-            if !url.rstrip.ends_with?("/")
-                url + "/"
-            else
-                url
-            end
-        end
-    end
-
+    
     server = HTTP::Server.new([
         HTTP::ErrorHandler.new,
         HTTP::LogHandler.new,
