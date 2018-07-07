6389dc389d6ab7dca6d77871d5897184f56c3c4c
Rishav <rishav.sharan@live.com>
2018-07-07 19:54:52 +0530

    WIP: dynamic routes

--- src/cove/routes.cr
+++ src/cove/routes.cr
@@ -8,6 +8,22 @@ module Cove
         property data :         Hash(String, String) = {"none" => "none"}
         property currentuser :  Hash(String, String) = {"loggedin" => "none", "unqid" => "none", "username" => "none" }
     end
+    class Route 
+        property resource :       String = ""
+        property identifier :     String = ""
+        property verb :         String = ""
+        
+        def initialize(url : String)
+            # Remove all leading and trailing whitespaces.
+            url = url.lstrip("/").rstrip(" / ")
+            arr = url.split("/")
+
+            @resource =     arr[0]? ? arr[0] : ""
+            @identifier =   arr[1]? ? arr[1] : ""
+            @verb =         arr[2]? ? arr[2] : ""
+
+        end
+    end
 
     class Router
         def self.run(method, url, ctx)
@@ -16,82 +32,16 @@ module Cove
             # path = {resource, item, method}
             store = Store.new
             store.currentuser = Cove::Auth.check(ctx)
-            pp url.split("/")
+            
+            rt = Route.new(url)
+            pp rt
+
             case path
-            when {"/about/",    "GET"}
-                ctx.response.content_type = "text/html; charset=utf-8"    
-                ctx.response.print Cove::Layout.render(Cove::Views.about(store), store)
-            when {"/noanon/",    "GET"}
-                guard("anon", store.currentuser["loggedin"], ctx)
-                ctx.response.content_type = "text/html; charset=utf-8"    
-                ctx.response.print "Yo"
-            when {"/nologgy/",    "GET"}
-                guard("user", store.currentuser["loggedin"], ctx)
-                ctx.response.content_type = "text/html; charset=utf-8"    
-                ctx.response.print "Yo"
-            when {"/secret/",     "GET"}
-                if store.currentuser["loggedin"] == "true"
-                    ctx.response.print "Yo #{ store.currentuser["username"] }! This secret is yours!"
-                else
-                    ctx.response.print "Sorry anon. This secret isn't meant for you!"
-                end
-
-                # Routes for Posts resource
-            when {"/post/new/", "GET"}
-                guard("anon", store.currentuser["loggedin"], ctx)
-                ctx.response.content_type = "text/html; charset=utf-8"    
-                ctx.response.print Cove::Layout.render(Cove::Views.new_post(store), store)
-            when {"/post/new/", "POST"}
-                guard("anon", store.currentuser["loggedin"], ctx)
-                ctx.response.content_type = "text/html; charset=utf-8"   
-                payload = Cove::Posts.create(ctx, store.currentuser["unqid"])
-                store.status =      payload.status
-                store.message =     payload.message
-                store.data =        payload.data
-                if  store.status == "error"
-                    ctx.response.print Cove::Layout.render(Cove::Views.new_post(store), store)
-                else
-                    ctx.response.print store
-                end
-                
-            # Routes for Register resource
-            when {"/register/", "GET"}
-                ctx.response.content_type = "text/html; charset=utf-8"    
-                ctx.response.print Cove::Layout.render(Cove::Views.register(store), store)
-            when {"/register/", "POST"}
-                ctx.response.content_type = "text/html; charset=utf-8"   
-                payload = Cove::Auth.register(ctx)
-                store.status =      payload.status
-                store.message =     payload.message
-                store.data =        payload.data
-                if  store.status == "error"
-                    ctx.response.print Cove::Layout.render(Cove::Views.register(store), store)
-                else
-                    ctx.response.print store
-                end
-
-            # Routes for Login resource
-            when {"/login/", "GET"}
-                ctx.response.content_type = "text/html; charset=utf-8"    
-                ctx.response.print Cove::Layout.render(Cove::Views.login(store), store)
-            when {"/login/", "POST"}
-                ctx.response.content_type = "text/html; charset=utf-8"   
-                payload =  Cove::Auth.login(ctx)
-                store.status =      payload.status
-                store.message =     payload.message
-                store.data =        payload.data
-                if store.status == "error"
-                    ctx.response.print Cove::Layout.render(Cove::Views.login(store), store)
-                else
-                    usercookie = HTTP::Cookie.new("usertoken", store.data["token"], "/", Time.now + 12.hours)
-                    ctx.response.headers["Set-Cookie"] = usercookie.to_set_cookie_header
-                    redirect("/", ctx)
-                end
-            when {"/logout/", "GET"}
-                usercookie = HTTP::Cookie.new("usertoken", "none", "/", Time.now + 12.hours)
-                ctx.response.headers["Set-Cookie"] = usercookie.to_set_cookie_header
-                redirect("/", ctx)
-
+            when {"about", "", "", "GET"}
+            when {"post", rt.identifier, "", "GET"}
+            when {"post", rt.identifier, "edit", "GET"}
+            when {"post", rt.identifier, "", "GET"}
+            when {"/about/", "", "", "GET"}
             # Catch-all routes    
             when {"/", "GET"}
                 ctx.response.content_type = "text/html; charset=utf-8"    
@@ -100,6 +50,89 @@ module Cove
                 ctx.response.content_type = "text/html; charset=utf-8"    
                 ctx.response.print Cove::Layout.render("404! Bewarsies! This be wasteland!", store)
             end
+            # case path
+            # when {"/about/",    "GET"}
+            #     ctx.response.content_type = "text/html; charset=utf-8"    
+            #     ctx.response.print Cove::Layout.render(Cove::Views.about(store), store)
+            # when {"/noanon/",    "GET"}
+            #     guard("anon", store.currentuser["loggedin"], ctx)
+            #     ctx.response.content_type = "text/html; charset=utf-8"    
+            #     ctx.response.print "Yo"
+            # when {"/nologgy/",    "GET"}
+            #     guard("user", store.currentuser["loggedin"], ctx)
+            #     ctx.response.content_type = "text/html; charset=utf-8"    
+            #     ctx.response.print "Yo"
+            # when {"/secret/",     "GET"}
+            #     if store.currentuser["loggedin"] == "true"
+            #         ctx.response.print "Yo #{ store.currentuser["username"] }! This secret is yours!"
+            #     else
+            #         ctx.response.print "Sorry anon. This secret isn't meant for you!"
+            #     end
+
+            #     # Routes for Posts resource
+            # when {"/post/new/", "GET"}
+            #     guard("anon", store.currentuser["loggedin"], ctx)
+            #     ctx.response.content_type = "text/html; charset=utf-8"    
+            #     ctx.response.print Cove::Layout.render(Cove::Views.new_post(store), store)
+            # when {"/post/new/", "POST"}
+            #     guard("anon", store.currentuser["loggedin"], ctx)
+            #     ctx.response.content_type = "text/html; charset=utf-8"   
+            #     payload = Cove::Posts.create(ctx, store.currentuser["unqid"])
+            #     store.status =      payload.status
+            #     store.message =     payload.message
+            #     store.data =        payload.data
+            #     if  store.status == "error"
+            #         ctx.response.print Cove::Layout.render(Cove::Views.new_post(store), store)
+            #     else
+            #         ctx.response.print store
+            #     end
+                
+            # # Routes for Register resource
+            # when {"/register/", "GET"}
+            #     ctx.response.content_type = "text/html; charset=utf-8"    
+            #     ctx.response.print Cove::Layout.render(Cove::Views.register(store), store)
+            # when {"/register/", "POST"}
+            #     ctx.response.content_type = "text/html; charset=utf-8"   
+            #     payload = Cove::Auth.register(ctx)
+            #     store.status =      payload.status
+            #     store.message =     payload.message
+            #     store.data =        payload.data
+            #     if  store.status == "error"
+            #         ctx.response.print Cove::Layout.render(Cove::Views.register(store), store)
+            #     else
+            #         ctx.response.print store
+            #     end
+
+            # # Routes for Login resource
+            # when {"/login/", "GET"}
+            #     ctx.response.content_type = "text/html; charset=utf-8"    
+            #     ctx.response.print Cove::Layout.render(Cove::Views.login(store), store)
+            # when {"/login/", "POST"}
+            #     ctx.response.content_type = "text/html; charset=utf-8"   
+            #     payload =  Cove::Auth.login(ctx)
+            #     store.status =      payload.status
+            #     store.message =     payload.message
+            #     store.data =        payload.data
+            #     if store.status == "error"
+            #         ctx.response.print Cove::Layout.render(Cove::Views.login(store), store)
+            #     else
+            #         usercookie = HTTP::Cookie.new("usertoken", store.data["token"], "/", Time.now + 12.hours)
+            #         ctx.response.headers["Set-Cookie"] = usercookie.to_set_cookie_header
+            #         redirect("/", ctx)
+            #     end
+            # when {"/logout/", "GET"}
+            #     usercookie = HTTP::Cookie.new("usertoken", "none", "/", Time.now + 12.hours)
+            #     ctx.response.headers["Set-Cookie"] = usercookie.to_set_cookie_header
+            #     redirect("/", ctx)
+
+            # # Catch-all routes    
+            # when {"/", "GET"}
+            #     ctx.response.content_type = "text/html; charset=utf-8"    
+            #     ctx.response.print Cove::Layout.render(Cove::Views.home(store), store)
+            # else
+            #     ctx.response.content_type = "text/html; charset=utf-8"    
+            #     ctx.response.print Cove::Layout.render("404! Bewarsies! This be wasteland!", store)
+            # end
         end
 
         def self.guard ( against, isloggedin, ctx)
